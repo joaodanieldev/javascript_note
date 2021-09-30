16957dd5e80e7adf958d60eb7ea63347de8ba75f
joaodanieldev@gmail.com <joaodanieldev@gmail.com>
2021-09-30 11:14:23 -0300

    estrutra do projeto global

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-import React from 'react';
-
-const App = () => <p>JS Note</p>
-
+import React, { Fragment } from 'react';
+import "./App.scss";
+import { Notification, Section } from 'rbx';
+ 
+const App = () => (
+  <Fragment>
+    <Section>
+      <Notification color="success"> 
+        JS NOTE CLIENTE!
+      </Notification>
+    </Section>
+  </Fragment>
+)
+ 
 export default App;
