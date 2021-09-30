6d8ab134203f44b2ed5f05d21f59dd986ab2758f
joaodanieldev@gmail.com <joaodanieldev@gmail.com>
2021-09-30 12:21:10 -0300

    router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Fragment } from 'react';
 import "./App.scss";
 import { Notification, Section } from 'rbx';
 import HomeScreen from './screens/home';
+import Routes from './routes';
  
 const App = () => (
   <Fragment>
-    <HomeScreen />
+    <Routes />
   </Fragment>
 )
  
