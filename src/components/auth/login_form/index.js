9c373529fbbe2bed7f86f8ab9d676fe128c7ddf0
joaodanieldev@gmail.com <joaodanieldev@gmail.com>
2021-10-05 12:15:12 -0300

    atualizaçao da conexao api

--- src/components/auth/login_form/index.js
+++ src/components/auth/login_form/index.js
@@ -1,53 +1,79 @@
-import { Button, Column, Control, Field, Help, Input, Label } from "rbx";
-import React, { Fragment, useState} from "react";
+import React, { Fragment, useState } from 'react';
+import { Button, Field, Control, Input, Column, Section, Help, Label } from "rbx";
 import { Redirect } from "react-router-dom";
+import UsersService from '../../../services/users';
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [redirectToRegister, setRedirectToRegister] = useState(false);
   const [redirectToNotes, setRedirectToNotes] = useState(false);
-  
   const [error, setError] = useState(false);
 
-  if(redirectToRegister )
+
+  const HandleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const user = await UsersService.login({email: email,password: password});
+      setRedirectToNotes(true);
+    } catch (error) {
+      setError(true);
+    }
+  }
+
+  if(redirectToRegister)
     return <Redirect to={{pathname: "/register"}}/>
   else if(redirectToNotes)
     return <Redirect to={{pathname: "/notes"}}/>
 
-
-  return(
-   <Fragment>
-     <Column.Group centered>
-       <Column size={12}>
-         <Field>
-           <Label size="smal">Email: </Label>
-           <Control>
-             <Input type="email" required name="email" value={email} onChange={e => setEmail(e.target.value)} />
-           </Control>
-         </Field>
-         <Field>
-           <Label size="small">Senha: </Label>
-           <Control>
-             <Input type="password" required name="password" value={password} onChange={e => setPassword(e.target.value)} />
-           </Control>
-         </Field>
-         <Field>
-           <Control>
-             <Column.Group breakpoint="mobile">
-               <Column>
-                <a className="button is-white has-text-custom-purple" onClick={e => setRedirectToRegister(true)} >Registrar</a>
-               </Column>
-               <Column>
-                    <Button color="custom-purple" outlined onClick={e => setRedirectToNotes(true)}>Entrar</Button>
+  return (
+    <Fragment>
+      <Column.Group centered>
+        <form onSubmit={HandleSubmit}>
+          <Column size={12}>
+            <Field>
+              <Label size="small">Email:</Label>
+              <Control>
+                <Input 
+                  type="email" 
+                  required
+                  name="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label size="small">Senha:</Label>
+              <Control>
+                <Input 
+                  type="password" 
+                  required
+                  name="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Control>
+                <Column.Group breakpoint="mobile">
+                  <Column>
+                    <a className="button is-white has-text-custom-purple"
+                       onClick={e => setRedirectToRegister(true)}
+                    >Register or</a>
                   </Column>
-             </Column.Group>
-           </Control>
-         </Field>
-         { error && <Help color="danger">Email ou senha invalido</Help> }
-       </Column>
-     </Column.Group>
-   </Fragment>
+                  <Column>
+                    <Button color="custom-purple" outlined>Login</Button>
+                  </Column>
+                </Column.Group>
+              </Control>
+            </Field>
+            { error && <Help color="danger">Email ou senha invalido</Help> }
+          </Column>
+        </form>
+      </Column.Group>
+    </Fragment>
   )
 }
 
