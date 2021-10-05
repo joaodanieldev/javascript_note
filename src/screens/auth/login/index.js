aa9da371c74450b27c43d15268dc303a005d08e5
joaodanieldev@gmail.com <joaodanieldev@gmail.com>
2021-10-05 11:16:32 -0300

    criar a tela do login

--- src/screens/auth/login/index.js
+++ src/screens/auth/login/index.js
@@ -1,8 +1,39 @@
+import { Card, Column, Container, Section, Title } from "rbx";
 import React, {Fragment} from "react";
+import Header from "../../../components/header";
+import logoImage from '../../../assets/images/logo.png';
+import '../../../styles/auth.scss';
 
 const LoginScreen = () => (
   <Fragment>
-    Bem vindo
+    <Header />
+    <Section size="medium" className="auth">
+      <Container>
+        <Column.Group centered>
+          <Column size={3}>
+            <Card>
+              <Card.Content>
+                <Section>
+                  <Column.Group centered>
+                    <Column size={12}>
+                      <img src={logoImage} />
+                    </Column>
+                  </Column.Group>
+                  <Column.Group>
+                    <Column size={12}>
+                      <Title size={6} className="has-text-grey has-text-centered">
+                        Suas notas na nuvem
+                      </Title>
+                    </Column>
+                  </Column.Group>
+                </Section>
+                
+              </Card.Content>
+            </Card>
+          </Column>
+        </Column.Group>
+      </Container>
+    </Section>
   </Fragment>
 )
 
