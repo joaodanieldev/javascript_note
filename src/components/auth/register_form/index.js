8c70c123f2b9c141c687bd1b1b2c0564af5cc005
joaodanieldev@gmail.com <joaodanieldev@gmail.com>
2021-10-05 10:45:03 -0300

    conexao api

--- src/components/auth/register_form/index.js
+++ src/components/auth/register_form/index.js
@@ -1,53 +1,84 @@
-import React, {Fragment, useState} from "react";
+import React, { Fragment, useState } from 'react';
 import { Button, Field, Control, Input, Column, Section, Help, Label } from "rbx";
 import { Redirect } from "react-router-dom";
+import UsersService from '../../../services/users';
 
-function RegisterForm () {
+
+function RegisterForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [redirectToLogin, setredirectToLogin] = useState(false);
+  const [redirectToLogin, setRedirectToLogin] = useState(false);
   const [error, setError] = useState(false);
 
-  if (redirectToLogin)
-    return <Redirect to={{pathname: "/login"}} />
+  const HandleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const user = await UsersService.register({name: name,email: email,password: password});
+      setRedirectToLogin(true);
+    } catch (error) {
+      setError(true);
+    }
+  }
 
-  return(
+
+  if(redirectToLogin)
+    return <Redirect to={{pathname: "/login"}}/>
+
+  return (
     <Fragment>
       <Column.Group centered>
-        <form>
+        <form onSubmit={HandleSubmit}>
           <Column size={12}>
             <Field>
               <Label size="small">Nome:</Label>
               <Control>
-                <Input type="name" required name="name" value={name} onChange={e => setName(e.target.value)} />
+                <Input
+                  type="name"
+                  required
+                  name="name"
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                />
               </Control>
             </Field>
             <Field>
-              <Label size="small">Email</Label>
+              <Label size="small">Email:</Label>
               <Control>
-                <Input type="email" required name="email" value={email} onChange={e => setEmail(e.target.value)} />
+                <Input 
+                  type="email" 
+                  required
+                  name="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
               </Control>
             </Field>
             <Field>
-              <Label size="small">Senha</Label>
+              <Label size="small">Senha:</Label>
               <Control>
-                <Input type="password" required name="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                <Input 
+                  type="password" 
+                  required
+                  name="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
               </Control>
             </Field>
             <Field>
               <Control>
                 <Column.Group breakpoint="mobile">
                   <Column>
-                    <a className="button is-white has-text-custom-purple" onClick={e => setredirectToLogin(true)}>Login ou</a>
+                    <a className="button is-white has-text-custom-purple" onClick={e => setRedirectToLogin(true)}>Login ou</a>
                   </Column>
                   <Column>
-                    <Button color="custom-purple" outlined> Registrar</Button>
+                    <Button color="custom-purple" outlined>Registrar</Button>
                   </Column>
                 </Column.Group>
               </Control>
             </Field>
-            { error && <Help color="danger">Email ou senha invalido!</Help> }
+            { error && <Help color="danger">Email ou senha invalido</Help> }
           </Column>
         </form>
       </Column.Group>
