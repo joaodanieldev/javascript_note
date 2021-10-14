4b7827b03834f7c59612f05ebb395c5b3bfcaaf4
joaodanieldev@gmail.com <joaodanieldev@gmail.com>
2021-10-14 13:01:53 -0300

    listar as notas

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from 'react';
 import HeaderLogged from '../../../components/header_logged';
 import Notes from '../../../components/notes';
 
+
 const NotesScreen = () => {
   const [isOpen, setIsOpen] = useState(false);
 
