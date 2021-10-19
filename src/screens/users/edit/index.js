a0a8d458c1d582c98f7ff2d14067da4374806f4f
joaodanieldev@gmail.com <joaodanieldev@gmail.com>
2021-10-19 13:51:04 -0300

    tela sistema usarios edit

--- src/screens/users/edit/index.js
+++ src/screens/users/edit/index.js
@@ -1,9 +1,47 @@
+import { Card, Column, Container, Section, Title } from "rbx";
 import React, {Fragment} from "react";
+import HeaderLogged from '../../../components/header_logged';
+import "../../../styles/users.scss";
 
 const UserEditScreen = () => (
   <Fragment>
-    Bem vindo
+   <HeaderLogged />
+   <Section size="medium" className="users">
+     <Container>
+       <Column.Group centered classname="users-edit">
+         <Column size={4}>
+           <Title size={5} className="has-text-grey has-text-left">
+              Informaçoes Pessoais
+           </Title>
+           <Card>
+             <Card.Content>
+               Editar usuario
+             </Card.Content>
+           </Card>
+         </Column>
+       </Column.Group>
+
+       <Column.Group centered className="users-edit">
+         <Column size={4}>
+           <Title size={5} className="has-text-grey has-text-left">
+              Senha
+           </Title>
+           <Card>
+             <Card.Content>
+               Editar a senha
+             </Card.Content>
+           </Card>
+         </Column>
+       </Column.Group>
+       <Column.Group centered>
+         <Column size={4} className="has-text-right">
+            Deletar usuario
+         </Column>
+       </Column.Group>
+     </Container>
+
+   </Section>
   </Fragment>
-)
+);
 
 export default  UserEditScreen;
